9969b8f49d7e9310e7ca55daf825ca7d29c5abaa
Riley Hanson <hansonrz@vcu.edu>
2017-01-16 16:53:31 -0500

    Uploaded Project Files

---
+++ CMSC506_FTP_TCP/src/ftp/FTPMain.java
@@ -0,0 +1,12 @@
+package ftp;
+
+/**
+ * 
+ * @author rileyZ
+ *
+ */
+public class FTPMain {
+    public static void main(String[] args) throws Exception{
+        System.out.println("FTP program by Riley(12.01.04.001)");  
+    }
+}
